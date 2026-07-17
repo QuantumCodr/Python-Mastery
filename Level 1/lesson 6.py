7ac2b25a83ffce58e48c6c5f5d1ae46335709e28
Gleekan David Williams <williamsgleekan.dev@gmail.com>
2026-07-16 19:39:37 -0700

    Lesson 6

--- Level 1/lesson 6.py
+++ Level 1/lesson 6.py
@@ -1,4 +1,47 @@
+# Program: Comparison Operator
+# Author: David
+# Lesson: 6
 
+print(10 > 5)
+
+# 1. Equal To (==)
+print(5 == 5) # True
+print(5 == 8) # False
+
+# 2. Not Equal To (!=)
+print(10 != 2) # True
+print(100 != 10) # False
+
+# 3. Greater Than (>)
+print(20 > 10) # True
+print(5 > 8) # False
+
+# 4. Less Than (<) 
+print(2 < 8) # True
+print(6 < 5) # False
+
+# 5. Greater Than or Equal To (>=)
+print(18 >= 18) # True
+print(20 >= 18) # True
+print(15 >- 18) # False
+
+# 6. Less Than or Equal To (<=)
+print(5 <= 5) # True
+print(3 <= 5) # True
+print(8 <= 5) # False
+
+# Comparing Strings
+print("David" == "David") # True
+print("David" == "John") # False
+print("Python" == "python")
+
+# Comparing Variables
+age = 20
+print(age >= 28) # True
+
+# Storing Comparison Result
+is_adult = age >= 28
+print(f"Is adult: {is_adult}")
 
 # Excercise 1
 print(10 == 10) # True
