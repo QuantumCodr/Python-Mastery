b80b6235b81fa5417f2d54eb12483df6abdfe912
Gleekan David Williams <williamsgleekan.dev@gmail.com>
2026-08-04 15:29:22 -0700

    Object Oriented Programming

---
+++ level 1/lesson 26.py
@@ -0,0 +1,126 @@
+# Program: Object Oriented Programming
+# Author: David
+# Lesson: 26
+# Date: 03/08/2026
+
+# 1. Creating Your First Class
+class Student:
+    pass
+
+student1 = Student()
+
+# 2. Creating an Object
+print(student1)
+
+# 3. Creating Multiple Objects
+student2 = Student()
+student3 = Student()
+student4 = Student()
+
+print(student2)
+print(student3)
+print(student3)
+
+# 4. Objects are Variables
+class Computer:
+    pass
+class Car:
+    pass
+class BankAccount:
+    pass
+
+student = Student()
+computer = Computer()
+car = Car()
+account = BankAccount()
+
+# Exercise 1
+class Car:
+    pass
+car1 = Car()
+car2 = Car()
+car3 = Car()
+print(car1)
+print(car2)
+print(car3)
+
+# Exercise 2
+class Dog:
+    pass
+dog1 = Dog()
+dog2 = Dog()
+dog3 = Dog()
+dog4 = Dog()
+print(dog1)
+print(dog2)
+print(dog3)
+print(dog3)
+
+# Exercice 3
+class Computer:
+    pass
+product1 = Computer()
+product2 = Computer()
+product3 = Computer()
+print(product1)
+print(product2)
+print(product3)
+
+# Exercise 4
+class Book:
+    pass
+
+class Library:
+    pass
+
+class BankAccount:
+    pass
+
+class Employee:
+    pass
+
+class Customer:
+    pass
+
+class Order:
+    pass
+
+class Invoice:
+    pass
+
+class Shop:
+    pass
+
+class Hospital:
+    pass
+
+class Patient:
+    pass
+
+# Mini Project
+class Student:
+    pass
+student1 = Student()
+student2 = Student()
+student3 = Student()
+
+class Teacher:
+    pass
+teacher1 = Teacher()
+teacher2 = Teacher()
+
+class Course:
+    pass
+course1 = Course()
+course2 = Course()
+course3 = Course()
+course4 = Course()
+course5 = Course()
+
+class Department:
+    pass
+department1 = Department()
+
+class School:
+    pass
+school1 = School()
